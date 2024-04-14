470f5df62a2b81a3907bb1389d0113a6fdee5aaf
Eldar <95678389+eldar88e@users.noreply.github.com>
2024-04-14 13:48:50 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,14 @@
-function ListenLi(event, toDoList) {
-    let targetBtn = event.target;
-    if (targetBtn.dataset.type === 'remove' || (targetBtn.tagName === 'I' && targetBtn.parentElement.dataset.type === 'remove')) {
-        let parent = (targetBtn.dataset.type === 'remove') ? targetBtn.parentElement.parentElement.parentElement : targetBtn.parentElement.parentElement.parentElement.parentElement;
-        let idx = parseFloat(parent.dataset.index);
-        toDoList.splice(idx, 1);
-        localStorage.setItem('user', JSON.stringify(toDoList));
-        parent.remove();
-    } else if (targetBtn.dataset.type === 'success' || (targetBtn.tagName === 'I' && targetBtn.parentElement.dataset.type === 'success')) {
-        let parent = (targetBtn.dataset.type === 'success') ? targetBtn.parentElement.parentElement.parentElement : targetBtn.parentElement.parentElement.parentElement.parentElement;
-        let btn    = (targetBtn.dataset.type === 'success') ? targetBtn : targetBtn.parentElement;
-        let idx    = parseFloat(parent.dataset.index);
-        btn.classList.toggle('orange');
-        toDoList[idx].status = !toDoList[idx].status;
-        localStorage.setItem('user', JSON.stringify(toDoList));
-        parent.classList.toggle('decoration');
-    }
+function removeTask(parent, idx, toDoList) {
+    toDoList.splice(idx, 1);
+    localStorage.setItem('user', JSON.stringify(toDoList));
+    parent.remove();
+}
+
+function toggleTaskStatus(parent, idx, btn, toDoList) {
+    btn.classList.toggle('orange');
+    toDoList[idx].status = !toDoList[idx].status;
+    localStorage.setItem('user', JSON.stringify(toDoList));
+    parent.classList.toggle('decoration');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -24,20 +18,17 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('add-do').addEventListener('submit', function(event) {
         event.preventDefault();
         const userInput = document.getElementById('userInput').value;
-        let found       = toDoList.some(i => i.do === userInput);
         
-        if (found) {
-            alert('Элемент уже существует в списке дел!');
-        } else {
-            if (userInput !== '') {
-                let newTask = { do: userInput, status: false }; 
-                toDoList.push(newTask);
-                localStorage.setItem('user', JSON.stringify(toDoList));
-                let idx = toDoList.length - 1;
-                outputDiv.innerHTML += addTask(newTask, idx);
-                document.dispatchEvent(customEvent);
-            }
-        }
+        if (toDoList.some(i => i.do === userInput)) return alert('Элемент уже существует в списке дел!');
+        
+        if (userInput === '') return;
+        
+        let newTask = { do: userInput, status: false }; 
+        toDoList.push(newTask);
+        localStorage.setItem('user', JSON.stringify(toDoList));
+        let idx = toDoList.length - 1;
+        outputDiv.innerHTML += addTask(newTask, idx);
+        document.dispatchEvent(customEvent);
     });
     
     document.getElementById('clear').addEventListener('click', function() {
@@ -55,26 +46,34 @@ document.addEventListener('DOMContentLoaded', function() {
         let task  = item.do;
         let klassBtn = (item.status) ? ' orange' : '';
         return `<li class="collection-item${klass}" data-index="${idx}">
-            <div class="task">
-                <span>${task}</span>
-                
-                <div class="btns">
-                <button class="waves-effect waves-light btn-small red" data-type="remove">
-                    <i class="material-icons">delete</i>
-                </button>
-                
-                <button class="waves-effect waves-light btn-small${klassBtn}" data-type="success">
-                    <i class="material-icons">check</i>
-                </button>
-                </li>
-                </div>
-                <div>`;
+                    <div class="task">
+                        <div class="title">${task}</div>
+                        
+                        <div class="btns">
+                            <button class="waves-effect waves-light btn-small red" data-type="remove">
+                                <i class="material-icons">delete</i>
+                            </button>
+                            
+                            <button class="waves-effect waves-light btn-small${klassBtn}" data-type="success">
+                                <i class="material-icons">check</i>
+                            </button>
+                        </div>
+                    </div>
+                </li>`;
     }
     
     
     outputDiv.addEventListener('click', function(event) {
-       ListenLi(event, toDoList);
-    }); 
+        let targetBtn = event.target.closest('button');
+        let parent = targetBtn.closest('li');
+        let idx = parseFloat(parent.dataset.index);
+
+        if (targetBtn.dataset.type === 'remove') {
+            removeTask(parent, idx, toDoList);
+        } else if (targetBtn.dataset.type === 'success') {
+            toggleTaskStatus(parent, idx, targetBtn, toDoList);
+        }
+    });  
 });
 
 let customEvent = new Event('myCustomEvent');
